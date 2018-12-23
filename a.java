2e280d1cb006215d3faee6f7000cc5ca074fa04b
itoysk <itoysk@sina.com>
2018-12-23 14:32:10 +0800

    dev

--- a.java
+++ a.java
@@ -3,4 +3,5 @@ public static void main(String[] args) {
     System.out.println("Hello World2!");
     System.out.println("Hello World3!");
     System.out.println("H4");
+    System.out.println("H5");
 }
