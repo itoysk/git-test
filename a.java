fbc80f0181a46926668244a6f299333b6e14afac
itoysk <itoysk@sina.com>
2018-12-23 14:38:17 +0800

    dev comm2018

--- a.java
+++ a.java
@@ -4,4 +4,5 @@ public static void main(String[] args) {
     System.out.println("Hello World3!");
     System.out.println("H4");
     System.out.println("H5");
+    System.out.println("201888888");
 }
